a9dd93c794f21bca9f62ece4b9a4c61633cf5d3e
Roenke <roenke54@gmail.com>
2016-03-20 19:21:41 +0300

    Add solution for task c.

--- contest1/src/c.cpp
+++ contest1/src/c.cpp
@@ -1,91 +1,83 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <set>
 
 typedef std::vector<std::vector<int>> graph_t;
-typedef std::vector<std::set<int>> brigdes_t;
 typedef std::vector<std::vector<int>> lca_t;
 
-void dfs(int vertex, int parent, graph_t const& graph, std::vector<uint8_t>& visited, std::vector<uint32_t>& time_in, std::vector<uint32_t>& fup, brigdes_t& brigdes,
-    std::vector<uint32_t>& deep)  {
-    static uint32_t timer = 0;
-    visited[vertex] = 1;
-    if(parent != -1) {
-        deep[vertex] = deep[parent] + 1;
-    }
+int timer = 0;
+std::vector<int> tin;
+std::vector<int> fup;
 
-    time_in[vertex] = fup[vertex] = timer++;
-    for(auto u : graph[vertex]) {
-        if (u == parent) continue;
-        if(visited[u]) {
-            fup[vertex] = std::min(fup[vertex], time_in[u]);
+// find bridges and parents
+void dfs1(int v, graph_t const& graph, std::vector<uint8_t>& visited, std::vector<int>& parent, std::vector<uint8_t>& brigde) {
+    visited[v] = 1;
+    tin[v] = fup[v] = timer++;
+    for(auto u : graph[v]) {
+        if (u == parent[v]) {
+            continue;
         }
-        else {
-            dfs(u, vertex, graph, visited, time_in, fup, brigdes, deep);
-            fup[vertex] = std::min(fup[vertex], fup[u]);
-            if (fup[u] > fup[vertex]) {
-                brigdes[u].insert(vertex);
-                brigdes[vertex].insert(u);
+
+        if(!visited[u]) {
+            parent[u] = v;
+            dfs1(u, graph, visited, parent, brigde);
+            fup[v] = std::min(fup[v], fup[u]);
+            if (fup[u] > tin[v]) {
+                brigde[u] = 1;
             }
         }
-    }
-}
-
-void build_spanning_tree(int vertex, graph_t const& g, std::vector<int>& spanning_tree,
-    std::vector<uint8_t>& visited, brigdes_t const& brigdes, std::vector<uint32_t>& b_count, uint32_t brigde_count) {
-    visited[vertex] = 1;
-    b_count[vertex] = brigde_count;
-    for(auto u : g[vertex]) {
-        if(!visited[u]) {
-            auto new_bridges_count = brigde_count + (brigdes[vertex].count(u) != 0 ? 1 : 0);
-            build_spanning_tree(u, g, spanning_tree, visited, brigdes, b_count, new_bridges_count);
-            spanning_tree[u] = vertex;
+        else {
+            fup[v] = std::min(fup[v], tin[u]);
         }
     }
 }
 
-void preprocess(int capital, graph_t const& g, std::vector<int>& spanning_tree, std::vector<uint32_t>& bridges_count_to_root, std::vector<uint32_t>& deep) {
-    int n = g.size();
-    std::vector<uint8_t> visited(n, 0);
-    std::vector<uint32_t> time_in(n, 0);
-    std::vector<uint32_t> fup(n, 0);
-    brigdes_t brigdes(n);
-    dfs(capital, -1, g, visited, time_in, fup, brigdes, deep);
-    
-    visited.assign(n, 0);
-    spanning_tree[capital] = capital;
-    build_spanning_tree(capital, g, spanning_tree, visited, brigdes, bridges_count_to_root, 0);
+// find deeps, bridges count
+void dfs2(int v, graph_t const& graph, std::vector<uint8_t>& visited, std::vector<int>& deep, std::vector<uint8_t> const& bridge, std::vector<int>& bridge_count) {
+    visited[v] = 1;
+    for(auto u : graph[v]) {
+        if(!visited[u]) {
+            deep[u] = deep[v] + 1;
+            bridge_count[u] = bridge_count[v];
+            if (bridge[u])
+                ++bridge_count[u];
+            dfs2(u, graph, visited, deep, bridge, bridge_count);
+        }
+    }
 }
 
-void build_lca(std::vector<int>& spanning_tree, lca_t& lca) {
-    int n = spanning_tree.size();
-    auto log_n = static_cast<int>(ceil(log2(n)) + 1);
+void build_lca(std::vector<std::vector<int>>& dp, int const log_n, std::vector<int> const& parent, int const capital) {
+    int n = dp.size();
     for (auto i = 0; i < n; ++i) {
-        lca[i].resize(log_n);
-        lca[i][0] = spanning_tree[i];
+        dp[i].resize(log_n);
+        dp[i][0] = parent[i];
+    }
+
+    for (auto i = 0; i < log_n; ++i) {
+        dp[capital][i] = capital;
     }
 
     for (auto j = 1; j < log_n; ++j) {
-        for (auto i = 1; i < n; ++i) {
-            lca[i][j] = lca[lca[i][j - 1]][j - 1];
+        for (auto i = 0; i < n; ++i) {
+            dp[i][j] = dp[dp[i][j - 1]][j - 1];
         }
     }
 }
 
-int get_lca(lca_t const& lca, std::vector<uint32_t>const& deep, int u, int v) {
+int get_lca(std::vector<std::vector<int>> const& dp, std::vector<int> const& deep, int u, int v) {
     if (u == v) {
         return v;
     }
 
-    const auto log_n = static_cast<int>(ceil(log2(lca.size())));
     if (deep[v] > deep[u]) {
-        std::swap(u, v);
+        std::swap(v, u);
     }
 
-    for (auto i = log_n; i >= 0; --i) {
+    int log_n = dp[0].size();
+
+    for (auto i = log_n - 1; i >= 0; --i) {
         if(deep[u] - deep[v] >= 1 << i) {
-            u = lca[u][i];
+            u = dp[u][i];
         }
     }
 
@@ -93,50 +85,58 @@ int get_lca(lca_t const& lca, std::vector<uint32_t>const& deep, int u, int v) {
         return v;
     }
 
-    for (auto i = log_n; i >= 0; --i) {
-        if(lca[v][i] != lca[u][i]) {
-            v = lca[v][i];
-            u = lca[u][i];
+    for (auto i = log_n - 1; i >= 0; --i) {
+        if (dp[v][i] != dp[u][i]) {
+            v = dp[v][i];
+            u = dp[u][i];
         }
     }
 
-    return lca[v][0];
+    return dp[v][0];
 }
 
 int main() {
+    lca_t lca;
     int n, m;
-    std::cin >> n >> m;
     int capital;
-    std::cin >> capital; --capital;
-    graph_t g(n);
 
-    int from, to;
+    std::cin.sync_with_stdio(false);
+    std::cout.sync_with_stdio(false);
+
+    std::cin >> n >> m >> capital; --capital;
+    graph_t graph(n);
+
+    int u, v;
     for (auto i = 0; i < m; ++i) {
-        std::cin >> from >> to; --from; --to;
-        g[from].push_back(to);
-        g[to].push_back(from);
+        std::cin >> u >> v; --u; --v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
     }
 
-    std::vector<int> spanning_tree(n);
-    std::vector<uint32_t> bridges_count_to_root(n, 0);
-    std::vector<uint32_t> deep(n);
-    deep[capital] = 0;
-    preprocess(capital, g, spanning_tree, bridges_count_to_root, deep);
+    tin.resize(n);
+    fup.resize(n);
+    std::vector<uint8_t> visited(n, 0);
+    std::vector<uint8_t> brigde(n, 0);
+    std::vector<int> parent(n, 0);
+    parent[capital] = capital;
+    dfs1(capital, graph, visited, parent, brigde);
 
-    lca_t lca(n);
-    build_lca(spanning_tree, lca);
+    visited.assign(n, 0);
+    std::vector<int> deep(n, 0);
+    std::vector<int> bridge_count(n, 0);
+    dfs2(capital, graph, visited, deep, brigde, bridge_count);
+
+    auto log_n = static_cast<int>(ceil(log2(n)));
+    std::vector<std::vector<int>> dp(n);
+    build_lca(dp, log_n, parent, capital);
 
     int k;
     std::cin >> k;
-
     std::vector<int> result(k);
-    int u, v;
     for (auto i = 0; i < k; ++i) {
         std::cin >> u >> v; --u; --v;
-        result[i] = bridges_count_to_root[get_lca(lca, deep, u, v)];
+        result[i] = bridge_count[get_lca(dp, deep, u, v)];
     }
 
     std::for_each(result.begin(), result.end(), [](int val) {std::cout << val << std::endl; });
-
-    return 0;
 }
