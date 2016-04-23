bce51b6cf863029c60f16af2f06a2183272823d0
Roenke <roenke54@gmail.com>
2016-04-23 13:26:58 +0300

    Add solution for task f.

--- contest2/src/f.cpp
+++ contest2/src/f.cpp
@@ -4,45 +4,85 @@
 #include <memory>
 #include <algorithm>
 const int INFTY = 100000000;
-using graph_t = std::vector<std::vector<std::pair<int, int>>>;
 
-struct cmp {
-    bool operator()(std::pair<int, int> const& left, std::pair<int, int> const& right) const {
-        return left.second > right.second;
+struct edge;
+using graph_t = std::vector<std::vector<std::shared_ptr<edge>>>;
+
+struct edge {
+    edge(int from, int to, int c, std::shared_ptr<edge> back)
+        : from(from),
+        to(to),
+        c(c),
+        f(0),
+        is_back(false),
+        back(back) {
     }
+
+    int from;
+    int to;
+    long long c;
+    bool is_back;
+    long long f;
+    std::shared_ptr<edge> back;
 };
 
-void dijkstra(int v, graph_t const& graph, std::vector<long long>& dist, std::vector<int>& prev) {
-    auto n = graph.size();
-    dist.clear(); dist.resize(n); dist.assign(n, INFTY);
-    prev.clear(); prev.resize(n); prev.assign(n, -1);
+long long dfs(int v, long long c_min, graph_t const& graph, std::vector<uint8_t>& visited, int to) {
+    if (v == to) {
+        return c_min;
+    }
 
-    dist[v] = 0;
-    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, cmp> queue;
-    queue.push(std::make_pair(v, 0));
-    while(!queue.empty()) {
-        auto p = queue.top(); queue.pop();
-        if (p.second > dist[p.first]) continue;
-        for(auto neig : graph[p.first]) {
-            auto new_dist = dist[p.first] + neig.second;
-            if(new_dist < dist[neig.first]) {
-                dist[neig.first] = new_dist;
-                prev[neig.first] = p.first;
-                queue.push({ neig.first, new_dist });
+    visited[v] = 1;
+    for (auto e : graph[v]) {
+        if (!visited[e->to] && e->f < e->c) {
+            auto d = dfs(e->to, std::min(c_min, e->c - e->f), graph, visited, to);
+            if (d > 0) {
+                e->f += d;
+                e->back->f -= d;
+                return d;
             }
+        }
+    }
+
+    return 0;
+}
+
+void dfs(int v, graph_t const& graph, std::vector<uint8_t>& visited, std::vector<std::shared_ptr<edge>>& prev, int to) {
+    if(v == to) {
+        return;
+    }
+
+    visited[v] = 1;
+    for(auto e : graph[v]) {
+        if(!visited[e->to] && e->f != 0) {
+            prev[e->to] = e;
+            if (e->to == to) return;
+            dfs(e->to, graph, visited, prev, to);
         }
     }
 }
 
-std::vector<int> build_path(int to, std::vector<int> const& prev) {
+std::vector<int> build_path(int to, std::vector<std::shared_ptr<edge>> const& prev) {
     std::vector<int> result;
-    auto current = to;
-    while (current != -1) {
-        result.push_back(current);
-        current = prev[current];
+    if (prev[to] == nullptr) return result;
+    auto current = prev[to];
+    result.push_back(current->to);
+    while (current != nullptr) {
+        current->f = current->back->f = 0;
+        result.push_back(current->from);
+        current = prev[current->from];
     }
+
     reverse(result.begin(), result.end());
     return result;
+}
+
+void print(std::vector<int> const& v) {
+    if (v.size() == 0) return;
+    for (auto i = 0; i < v.size() - 1; ++i) {
+        std::cout << v[i] + 1 << " ";
+    }
+
+    std::cout << v.back() + 1 << std::endl;
 }
 
 int main() {
@@ -54,42 +94,39 @@ int main() {
     for (size_t i = 0; i < m; ++i) {
         int from, to;
         cin >> from >> to; --from; --to;
-        graph[from].emplace_back(to, 1);
+        if (from == to) continue;
+        auto e1 = make_shared<edge>(from, to, 1, nullptr);
+        auto e2 = make_shared<edge>(to, from, 0, e1);
+        e2->is_back = true;
+        e1->back = e2;
+        graph[from].push_back(e1);
+        graph[to].push_back(e2);
     }
 
-    vector<long long> dist;
-    vector<int> prev;
-
-    dijkstra(apricot, graph, dist, prev);
-    if(dist[home] == INFTY) {
+    vector<uint8_t> visited(n, 0);
+    dfs(apricot, INFTY, graph, visited, home);
+    visited.assign(n, 0);
+    auto c = dfs(apricot, INFTY, graph, visited, home);
+    if(c == 0) {
         cout << "NO" << endl;
         return 0;
     }
 
-    auto path1(build_path(home, prev));
+    vector<shared_ptr<edge>> prev(n, nullptr);
+    visited.assign(n, 0);
+    dfs(apricot, graph, visited, prev, home);
+    auto p1 = build_path(home, prev);
+    prev.assign(n, nullptr);
+    visited.assign(n, 0);
+    dfs(apricot, graph, visited, prev, home);
+    auto p2 = build_path(home, prev);
 
-    for (size_t i = 0; i < path1.size() - 1; ++i) {
-        size_t ix = path1[i + 1];
-        auto pos = std::find_if(graph[path1[i]].begin(), graph[path1[i]].end(), [ix](pair<int, int> const& p) { return p.first == ix; });
-        pos->second = 2 * n;
-    }
-
-    dijkstra(apricot, graph, dist, prev);
-    if(dist[home] > n) {
-        cout << "NO" << endl;
-        return 0;
-    }
-
-    auto path2(build_path(home, prev));
+    //if(p1.size() == 0 || p2.size() == 0) {
+    //    cout << "NO" << endl;
+    //    return 0;
+    //}
 
     cout << "YES" << endl;
-    for(auto v : path1) {
-        std::cout << v + 1 << " ";
-    }
-    cout << endl;
-
-    for (auto v : path2) {
-        std::cout << v + 1 << " ";
-    }
-    cout << endl;
+    print(p1);
+    print(p2);
 }
